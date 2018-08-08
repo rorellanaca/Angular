c3d01e74298250f78312bb0c99d081e5eba1b780
reus <rorellana3004@gmail.com>
2018-08-07 21:38:07 -0600

    Nuevo componente e imagen para pipes

---
+++ 04-spotiapp/src/app/components/tarjetas/tarjetas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjetas',
+  templateUrl: './tarjetas.component.html',
+  styles: []
+})
+export class TarjetasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
