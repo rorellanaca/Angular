79b1302a0af9b380c25c8c20b15e33523a779d19
rorellanaca <40066722+rorellanaca@users.noreply.github.com>
2018-09-07 15:20:42 -0600

    modificacion a terminadas

--- 05-tareas/src/pages/teminados/terminados.component.ts
+++ 05-tareas/src/pages/teminados/terminados.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { TareasService } from '../../services/tareas.service';
 import { Lista } from '../../models/lista.model';
+import { NavController, AlertController } from 'ionic-angular';
+import { AgregarPage } from '../agregar/agregar.component';
 
 @Component({
     selector: 'page-terminados',
@@ -9,10 +11,15 @@ import { Lista } from '../../models/lista.model';
 
 export class TerminadosPage {
 
-  constructor(public tareasService: TareasService){}
+  constructor(public tareasService: TareasService,
+                private navCtrl: NavController,
+                private alertCtrl: AlertController){}
 
-  listaSeleccionada(lista: Lista){
-      console.log(lista);
-  }
+    listaSeleccionada(lista: Lista){
+        this.navCtrl.push( AgregarPage,{
+            titulo: lista.titulo,
+            Lista: lista
+        });     
+    }
 
 }
